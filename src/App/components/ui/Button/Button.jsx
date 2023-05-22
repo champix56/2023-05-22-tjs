13cedc235e27cef572b546f1c0fd7402cbae35f4
champix56 <desorbaix@free.fr>
2023-05-22 13:46:42 +0100

    gestion module css

--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import './Button.css'
+import styles from './Button.module.css'
+
 const Button=(props)=>{
-    return <button className='Button'>{props.children}</button>
+    return <button onClick={(evt)=>{
+        props.onButtonClick('hello');
+    }} className={styles.Button}>{props.children}</button>
 }
 
 export default Button; 
