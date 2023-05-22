e7612fcc4b6bc18f7d479a33bc395c676bc8cd5e
champix56 <desorbaix@free.fr>
2023-05-22 15:31:29 +0100

    passage ts du button

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
-import { MemeSVGViewer, emptyMeme } from 'orsys-tjs-meme';
 import Button from './components/ui/Button/Button';
 
 function App() {
   return (
     <div className="App">
       <Button style={{ textDecoration: 'underline' }} onButtonClick={(unTruc) => {
-        console.log('%c%s', 'font-size:28pt;color:tomato;text-decoration:underline blue;', unTruc);
-      }}>
-        ok<img src='https://cdn4.iconfinder.com/data/icons/icocentre-free-icons/137/f-check_256-256.png' alt='' />
-      </Button>
+        console.log(unTruc);
+      }} >ok</Button>
     </div>
   );
 }
--- src/App/components/ui/Button/Button.tsx
+++ src/App/components/ui/Button/Button.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import styles from './Button.module.css'
 import PropTypes from 'prop-types'
-const Button = (props) => {
-    return <button onClick={(evt) => {
+interface I_ButtonProps{
+    onButtonClick: Function
+    style?:object
+    bgColor?:string
+    children:string|React.ReactNode|Array<React.ReactNode|string>
+    type:'button'|'reset'|'submit'
+}
+
+const Button:React.FC<I_ButtonProps> = (props) => {
+    return <button type={props.type} onClick={(evt) => {
         props.onButtonClick('hello');
     }} className={styles.Button}
         style={{ ...props.style,backgroundColor: props.bgColor }}
