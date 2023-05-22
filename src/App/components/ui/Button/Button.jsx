bc2e979470386a9186040631a96e33eba2818ec2
champix56 <desorbaix@free.fr>
2023-05-22 14:59:37 +0100

    proptypes

--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Button.module.css'
-
+import PropTypes from 'prop-types'
 const Button = (props) => {
     return <button onClick={(evt) => {
         props.onButtonClick('hello');
@@ -8,5 +8,10 @@ const Button = (props) => {
         style={{ ...props.style,backgroundColor: props.bgColor }}
     >{props.children}</button>
 }
-
+Button.propTypes={
+    children: PropTypes.any.isRequired,
+    onButtonClick: PropTypes.func.isRequired,
+    bgColor: PropTypes.string,
+    style: PropTypes.object
+}
 export default Button; 
