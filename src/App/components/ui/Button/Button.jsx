53195569f61569b464e9dea64eef3e24fa0e1d30
champix56 <desorbaix@free.fr>
2023-05-22 13:19:01 +0100

    ajout css

--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import './Button.css'
 const Button=(props)=>{
-    return <button>{props.children}</button>
+    return <button className='Button'>{props.children}</button>
 }
 
 export default Button; 
