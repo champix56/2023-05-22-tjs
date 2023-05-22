ac1b6d96b5159fbee7d745242c04bfe11dbc49b8
champix56 <desorbaix@free.fr>
2023-05-22 11:12:02 +0100

    usage d'un composant

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { MemeSVGViewer, emptyMeme } from 'orsys-tjs-meme';
 
 function App() {
   return (
     <div className="App">
-      DEMAT BREIZH
+      <MemeSVGViewer meme={emptyMeme} image={undefined} />
     </div>
   );
 }
