e0883d076c932e41c786aca50db9212cb0fb2104
champix56 <desorbaix@free.fr>
2023-05-22 13:13:14 +0100

    component wrapper

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,7 +6,15 @@ import Button from './components/ui/Button/Button';
 function App() {
   return (
     <div className="App">
-      <Button text="Texte du button" />
+      <Button children={
+      <>
+      <div>
+        ok</div>
+        <img src='https://cdn4.iconfinder.com/data/icons/icocentre-free-icons/137/f-check_256-256.png'/>
+        </>
+        }>
+    
+      </Button>
     </div>
   );
 }
