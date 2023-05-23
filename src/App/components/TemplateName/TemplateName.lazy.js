33e3db578766c495e68796621d076faa5cc79712
champix56 <desorbaix@free.fr>
2023-05-23 09:06:46 +0100

    test de grc

---
+++ src/App/components/TemplateName/TemplateName.lazy.js
@@ -0,0 +1,11 @@
+import React, { lazy, Suspense } from 'react';
+
+const LazyTemplateName = lazy(() => import('./TemplateName'));
+
+const TemplateName = props => (
+  <Suspense fallback={null}>
+    <LazyTemplateName {...props} />
+  </Suspense>
+);
+
+export default TemplateName;
---
+++ src/App/components/TemplateName/TemplateName.stories.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import TemplateName from './TemplateName';
+
+export default {
+  title: "TemplateName",
+};
+
+export const Default = () => <TemplateName />;
+
+Default.story = {
+  name: 'default',
+};
