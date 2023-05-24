c2ce00a0b811e31ccb4256233b0fb9d0e574e2a9
champix56 <desorbaix@free.fr>
2023-05-24 14:57:50 +0100

    connection du MemeForm au store

--- src/App/components/functionnal/MemeForm/StoreMemeForm.jsx
+++ src/App/components/functionnal/MemeForm/StoreMemeForm.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import MemeForm from './MemeForm'
-import {useDispatch, useSelector} from 'react-redux'
-import {changeMeme} from '../../../store/current.js'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeMeme, clearMeme, saveCurrent } from '../../../store/current.js'
 export default function StoreMemeForm() {
-    const fnStoreDispatch=useDispatch();
-    const imagesFromeStore=useSelector(s=>s.listes.images)
-    const onMemeChange=(meme)=>{
-        fnStoreDispatch(changeMeme(meme));
-        //changeMeme -> createur d'action pour gen {type:'current/change',payload:meme}
-    }
+  const fnStoreDispatch = useDispatch();
+  const imagesFromeStore = useSelector(s => s.listes.images)
+  const currentFromeStore = useSelector(s => s.current)
+  const onMemeChange = (meme) => {
+    fnStoreDispatch(changeMeme(meme));
+    //changeMeme -> createur d'action pour gen {type:'current/change',payload:meme}
+  }
   return (
-   <MemeForm images={imagesFromeStore} meme={meme} onMemeChange={onMemeChange}/>
+    <MemeForm
+      images={imagesFromeStore}
+      meme={currentFromeStore}
+      onMemeChange={onMemeChange}
+      onSaveMeme={(meme) => {
+        fnStoreDispatch(saveCurrent(meme));
+      }}
+      onClearMeme={() => {
+        fnStoreDispatch(clearMeme());
+      }} />
   )
 }
