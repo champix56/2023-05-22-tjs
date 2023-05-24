9f0f379f59448975501a360ea61b09c06587124f
champix56 <desorbaix@free.fr>
2023-05-24 14:55:59 +0100

    connection des fonction de save au store

--- src/App/pages/MemeEditor/MemeEditor.jsx
+++ src/App/pages/MemeEditor/MemeEditor.jsx
@@ -2,20 +2,28 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { MemeSVGViewer } from 'orsys-tjs-meme';
 import MemeForm from '../../components/functionnal/MemeForm/MemeForm';
-
+import StoreMemeForm from '../../components/functionnal/MemeForm/StoreMemeForm';
+import {useSelector} from 'react-redux'
 function MemeEditor(props) {
 
     return (<>
-        <MemeSVGViewer meme={meme} image={imgs.find((img) => img.id === meme.imageId)} basePath='' />
-        <MemeForm meme={meme} images={imgs} onMemeChange={(meme) => {
-            setmeme(meme);
-        }} />
+        <MemeViewer />
+        <StoreMemeForm />
     </>
     )
 }
-
 MemeEditor.propTypes = {
     id: PropTypes.number,
 }
 
 export default MemeEditor
+
+function MemeViewer() {
+
+    const currentFromeStore = useSelector(s => s.current)
+    const imageFromeStore = useSelector(s => s.listes.images.find((img) => img.id === currentFromeStore.imageId))
+    return (
+        <MemeSVGViewer meme={currentFromeStore} image={imageFromeStore} basePath='' />
+    )
+}
+
