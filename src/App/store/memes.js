5f18978d375ecfc5123eaae1c512f1540748d767
champix56 <desorbaix@free.fr>
2023-05-24 13:09:25 +0100

    tranche des liste + creation store

---
+++ src/App/store/memes.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+const initialState = {
+    images: [],
+    memes: [],
+}
+
+const memes = createSlice({
+    name: 'listes',
+    initialState,
+    reducers: {
+        addImage: (state, action) => {
+            state.images.push(action.payload);
+        },
+        addImages: (state, action) => {
+            state.images.push(...action.payload);
+        }
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(fetchAllListesValues.fulfilled,(state,action)=>{
+            state.images.push(...action.payload.images);
+            state.memes.push(...action.payload.memes);
+            console.log(state,action)
+        })
+        builder.addDefaultCase((state,action)=>{console.log(state,action)})
+    }
+});
+export const fetchAllListesValues=createAsyncThunk('listes/fetchAll', async () => {
+
+    const prImg = await fetch(`http://localhost:5679/images`);
+    const prMeme = await fetch(`http://localhost:5679/memes`);
+
+    const prs = await Promise.all([prImg, prMeme]);
+    const imgs = await prs[0].json();
+    const memes = await prs[1].json();
+    return { images: imgs, memes: memes };
+})
+export const { addImage, addImages } = memes.actions;
+
+export default memes.reducer;
--- src/App/store/store.js
+++ src/App/store/store.js
@@ -1,24 +1,8 @@
-import { createStore, combineReducers } from "redux"
-
-export const storeInitialState = { content: '', isShown: false }
-
-const reducer = (state = storeInitialState, action) => {
-    console.log(action);
-    switch (action.type) {
-        case 'modal/show':
-            return { ...state, content: action.payload, isShown: true }
-        case 'modal/hide':
-            return { ...state, content: '', isShown: false }
-        default:
-            return state
-    }
-}
-export const store=createStore(combineReducers({modal:reducer,modal2:reducer}));
-store.subscribe(()=>{console.log(store.getState())});
-
-store.dispatch(showModal('coucou'))
-store.dispatch({type:'modal/hide'})
-
-export function showModal(content){
-    return {type:'modal/show',payload:content}
-}
+import {configureStore,combineReducers} from '@reduxjs/toolkit'
+import listesReducer, { fetchAllListesValues } from './memes'
+export const store=configureStore({
+    reducer:combineReducers({listes:listesReduce}),
+    devTools:true
+});
+console.log('async thunk called')
+store.dispatch(fetchAllListesValues());
