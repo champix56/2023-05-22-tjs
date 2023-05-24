ee7480a4c47775863623f7a071c001809a88f656
champix56 <desorbaix@free.fr>
2023-05-24 13:10:18 +0100

    fix name error

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -1,7 +1,7 @@
 import {configureStore,combineReducers} from '@reduxjs/toolkit'
 import listesReducer, { fetchAllListesValues } from './memes'
 export const store=configureStore({
-    reducer:combineReducers({listes:listesReduce}),
+    reducer:combineReducers({listes:listesReducer}),
     devTools:true
 });
 console.log('async thunk called')
