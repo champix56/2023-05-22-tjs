fa0adbe56b6d66b0d3e93a86f5df34104934ba2e
champix56 <desorbaix@free.fr>
2023-05-24 14:57:35 +0100

    ajout de la slice current

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -1,7 +1,8 @@
 import {configureStore,combineReducers} from '@reduxjs/toolkit'
 import listesReducer, { fetchAllListesValues } from './memes'
+import currentReducer from './current'
 export const store=configureStore({
-    reducer:combineReducers({listes:listesReducer}),
+    reducer:combineReducers({listes:listesReducer,current:currentReducer}),
     devTools:true
 });
 console.log('async thunk called')
